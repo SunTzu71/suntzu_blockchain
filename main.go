f95a924914f5ed56b3432b8f4162654cb2747ad4
Chris Hall <chall0311@gmail.com>
2024-12-06 11:36:53 -0500

    Adding command line to start blockchain server and star mining

--- main.go
+++ main.go
@@ -1,14 +1,12 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"log"
 	"os"
-	"os/signal"
 
-	"github.com/SunTzu71/suntzu_blockchain/blockchain"
-	"github.com/SunTzu71/suntzu_blockchain/blockchainserver"
 	"github.com/SunTzu71/suntzu_blockchain/constants"
-	"github.com/SunTzu71/suntzu_blockchain/walletserver"
 )
 
 // Inialize blockchain name
@@ -19,17 +17,57 @@ func init() {
 // Main function to run the blockchain
 func main() {
 
-	genesisBlock := blockchain.NewBlock("0x0", 0)
-	blockchain1 := blockchain.NewBlockchain(*genesisBlock)
-	bcs := blockchainserver.CreateBlockchainServer(8000, blockchain1)
-	ws := walletserver.CreateWalletServer(8080, "http://127.0.0.1:8000")
+	chainCommandSet := flag.NewFlagSet("chain", flag.ExitOnError)
+	walletCommandSet := flag.NewFlagSet("wallet", flag.ExitOnError)
 
-	// Start the blockchain server and wallet server
-	go bcs.StartBlockchainServer()
-	go ws.StartWalletServer()
+	chainPort := chainCommandSet.Uint("port", 8000, "port to run the blockchain server")
+	chainMiner := chainCommandSet.String("miner", "", "miner address")
 
-	// Wait for interrupt signal
-	c := make(chan os.Signal, 1)
-	signal.Notify(c, os.Interrupt)
-	<-c
+	walletPort := walletCommandSet.Uint("port", 8080, "port to run the wallet server")
+	blockchainNodeAddress := walletCommandSet.String("node", "http://127.0.0.1:8000", "blockchain node address")
+
+	if len(os.Args) < 2 {
+		fmt.Println("Error: expected chain or wallet command")
+	}
+
+	switch os.Args[1] {
+	case "chain":
+		chainCommandSet.Parse(os.Args[2:])
+		if chaincommandSet.Parsed() {
+			if *chainMiner == "" || chainCommandSet.NFlag() == 0 {
+				fmt.Println("Usage of chain subcommand: ")
+				chainCommandSet.PrintDefaults()
+				os.Exit(1)
+			}
+			genesisBlock := blockchain.NewBlock("0x0", 0)
+			blockchain := blockchain.NewBlockchain(*genesisBlock)
+			bcs := blockchainserver.CreateBlockchainServer(chainPort, blockchain)
+			go bcs.StartBlockchainServer()
+			go bcs.BlockchainPtr.ProofOfWorkMining(*chainMiner)
+
+			// Wait for interrupt signal
+			c := make(chan os.Signal, 1)
+			signal.Notify(c, os.Interrupt)
+			<-c
+		}
+	}
+	case "wallet":
+	default:
+		fmt.Println("Error: expected chain or wallet command")
+		os.Exit(1)
+	}
+
+	// genesisBlock := blockchain.NewBlock("0x0", 0)
+	// blockchain1 := blockchain.NewBlockchain(*genesisBlock)
+	// bcs := blockchainserver.CreateBlockchainServer(8000, blockchain1)
+	// ws := walletserver.CreateWalletServer(8080, "http://127.0.0.1:8000")
+
+	// // Start the blockchain server and wallet server
+	// go bcs.StartBlockchainServer()
+	// go ws.StartWalletServer()
+
+	// // Wait for interrupt signal
+	// c := make(chan os.Signal, 1)
+	// signal.Notify(c, os.Interrupt)
+	// <-c
 }
